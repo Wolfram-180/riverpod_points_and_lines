d18dd536cf92f7e4caed3ba3b4e34b0c87b953df
Wolfram-180 <sergei.ulvis@gmail.com>
2024-03-14 17:17:48 +0300

    +lines intersections check

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,122 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: LineDrawingScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+final lineStateProvider =
+    StateNotifierProvider<LineStateNotifier, List<Offset>>(
+  (ref) => LineStateNotifier(),
+);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class LineStateNotifier extends StateNotifier<List<Offset>> {
+  LineStateNotifier() : super([]);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void addPoint(Offset point) {
+    if (state.isNotEmpty && _doesNewLineIntersectAny(point)) {
+      if (kDebugMode) {
+        print('Действие отменено - новая линия пересечет ранее созданную');
+      }
+    } else {
+      state = [...state, point];
+    }
   }
 
+  bool _doesNewLineIntersectAny(Offset newPoint) {
+    if (state.length < 2) {
+      return false; // нужно минимум 2 точки для сегмента
+    }
+
+    // новый сегмент от последней точки в списке - к новой точке
+    LineSegment newSegment = LineSegment(
+      state.last,
+      newPoint,
+    );
+
+    // проверка на пересечение с каждым сегментом в списке
+    for (int i = 0; i < state.length - 2; i++) {
+      LineSegment existingSegment = LineSegment(state[i], state[i + 1]);
+      if (newSegment.intersects(existingSegment)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
+
+class LineSegment {
+  Offset p1;
+  Offset p2;
+
+  LineSegment(this.p1, this.p2);
+
+  bool intersects(LineSegment other) {
+    double s1x, s1y, s2x, s2y;
+    s1x = p2.dx - p1.dx;
+    s1y = p2.dy - p1.dy;
+    s2x = other.p2.dx - other.p1.dx;
+    s2y = other.p2.dy - other.p1.dy;
+
+    double s, t;
+    s = (-s1y * (p1.dx - other.p1.dx) + s1x * (p1.dy - other.p1.dy)) /
+        (-s2x * s1y + s1x * s2y);
+    t = (s2x * (p1.dy - other.p1.dy) - s2y * (p1.dx - other.p1.dx)) /
+        (-s2x * s1y + s1x * s2y);
+
+    return s >= 0 && s <= 1 && t >= 0 && t <= 1;
+  }
+}
+
+class LineDrawingScreen extends ConsumerWidget {
+  const LineDrawingScreen({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context, WidgetRef ref) {
+    final linePoints = ref.watch(lineStateProvider);
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: GestureDetector(
+        onTapUp: (details) {
+          ref.read(lineStateProvider.notifier).addPoint(details.localPosition);
+        },
+        child: CustomPaint(
+          painter: LinePainter(linePoints),
+          size: Size.infinite,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class LinePainter extends CustomPainter {
+  final List<Offset> points;
+  LinePainter(this.points);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 4;
+    for (int i = 0; i < points.length - 1; i++) {
+      canvas.drawLine(points[i], points[i + 1], paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
